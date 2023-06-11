4ac274526c613a2830c9ec2347ccc8fc53e66f32
CalebOrange <1834962215@qq.com>
2023-06-11 17:04:50 +0800

    initial lab3

---
+++ src/backend/generator.cpp
@@ -0,0 +1,23 @@
+#include"backend/generator.h"
+
+#include<assert.h>
+
+
+#define TODO assert(0 && "todo")
+
+backend::Generator::Generator(ir::Program& p, std::ofstream& f): program(p), fout(f) {}
+
+void backend::Generator::gen() {
+    for(auto & global_val : program.global_vars) {
+        fout << ".global " << global_val.first << std::endl;
+        fout << ".align 2" << std::endl;
+        fout << ".type " << global_val.first << ", @object" << std::endl;
+        fout << ".size " << global_val.first << ", 4" << std::endl;
+        fout << global_val.first << ":" << std::endl;
+        fout << ".word " << global_val.second << std::endl;
+    }
+    for(auto & func : program.functions) {
+        gen_func(func);
+    }
+}
+
