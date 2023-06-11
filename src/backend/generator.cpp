347559129e0c60382cd13dab47b375a879c51557
CalebOrange <1834962215@qq.com>
2023-06-11 17:10:00 +0800

    lab3 update

--- src/backend/generator.cpp
+++ src/backend/generator.cpp
@@ -1,14 +1,15 @@
-#include"backend/generator.h"
+#include "backend/generator.h"
 
-#include<assert.h>
-
+#include <assert.h>
 
 #define TODO assert(0 && "todo")
 
-backend::Generator::Generator(ir::Program& p, std::ofstream& f): program(p), fout(f) {}
+backend::Generator::Generator(ir::Program &p, std::ofstream &f) : program(p), fout(f) {}
 
-void backend::Generator::gen() {
-    for(auto & global_val : program.global_vars) {
+void backend::Generator::gen()
+{
+    for (auto &global_val : program.globalVal)
+    {
         fout << ".global " << global_val.first << std::endl;
         fout << ".align 2" << std::endl;
         fout << ".type " << global_val.first << ", @object" << std::endl;
@@ -16,8 +17,11 @@ void backend::Generator::gen() {
         fout << global_val.first << ":" << std::endl;
         fout << ".word " << global_val.second << std::endl;
     }
-    for(auto & func : program.functions) {
+    for (auto &func : program.functions)
+    {
         gen_func(func);
     }
 }
 
+void backend::Generator::gen_func(const ir::Function &){}
+void backend::Generator::gen_instr(const ir::Instruction &){}
